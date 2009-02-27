4a7558279bbda33626f1d4ffeb4717164617e9ac
Esfand Shayan <eshayan@gmail.com>
2009-02-27 02:48:38 +0200

    TooltipDialogWTL added

---
+++ TooltipDialogWTL/MainDlg.h
@@ -0,0 +1,97 @@
+// MainDlg.h : interface of the CMainDlg class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class CMainDlg : public CDialogImpl<CMainDlg>, public CUpdateUI<CMainDlg>,
+		public CMessageFilter, public CIdleHandler,
+		public CToolTipDialog<CMainDlg>
+{
+public:
+	enum { IDD = IDD_MAINDLG };
+
+	virtual BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		return IsDialogMessage(pMsg);
+	}
+
+	virtual BOOL OnIdle()
+	{
+		return FALSE;
+	}
+
+	BEGIN_UPDATE_UI_MAP(CMainDlg)
+	END_UPDATE_UI_MAP()
+
+	BEGIN_MSG_MAP(CMainDlg)
+		COMMAND_HANDLER(IDC_ACTIVATE, BN_CLICKED, OnBnClickedActivate)
+		COMMAND_HANDLER(IDC_EDIT_TT, EN_CHANGE, OnEnChangeEditTt)
+		NOTIFY_HANDLER(IDC_SIZE_TT, NM_RELEASEDCAPTURE, OnNMReleasedcaptureSizeTt)
+		CHAIN_MSG_MAP(CToolTipDialog<CMainDlg>)
+		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
+		COMMAND_ID_HANDLER(ID_APP_ABOUT, OnAppAbout)
+		COMMAND_ID_HANDLER(IDOK, OnOK)
+		COMMAND_ID_HANDLER(IDCANCEL, OnCancel)
+	END_MSG_MAP()
+
+// Handler prototypes (uncomment arguments if needed):
+//	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+//	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+//	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+
+	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		// center the dialog on the screen
+		CenterWindow();
+
+		// set icons
+		HICON hIcon = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
+			IMAGE_ICON, ::GetSystemMetrics(SM_CXICON), ::GetSystemMetrics(SM_CYICON), LR_DEFAULTCOLOR);
+		SetIcon(hIcon, TRUE);
+		HICON hIconSmall = (HICON)::LoadImage(_Module.GetResourceInstance(), MAKEINTRESOURCE(IDR_MAINFRAME), 
+			IMAGE_ICON, ::GetSystemMetrics(SM_CXSMICON), ::GetSystemMetrics(SM_CYSMICON), LR_DEFAULTCOLOR);
+		SetIcon(hIconSmall, FALSE);
+
+		// register object for message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->AddMessageFilter(this);
+		pLoop->AddIdleHandler(this);
+
+		UIAddChildWindowContainer(m_hWnd);
+
+		CheckDlgButton(IDC_ACTIVATE,BST_CHECKED);
+
+		return TRUE;
+	}
+
+	LRESULT OnAppAbout(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		CAboutDlg dlg;
+		dlg.DoModal();
+		return 0;
+	}
+
+	LRESULT OnOK(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		// TODO: Add validation code 
+		CloseDialog(wID);
+		return 0;
+	}
+
+	LRESULT OnCancel(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+	{
+		CloseDialog(wID);
+		return 0;
+	}
+
+	void CloseDialog(int nVal)
+	{
+		DestroyWindow();
+		::PostQuitMessage(nVal);
+	}
+	LRESULT OnBnClickedActivate(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnEnChangeEditTt(WORD /*wNotifyCode*/, WORD wID, HWND /*hWndCtl*/, BOOL& /*bHandled*/);
+	LRESULT OnNMReleasedcaptureSizeTt(int idCtrl, LPNMHDR pNMHDR, BOOL& /*bHandled*/);
+};
